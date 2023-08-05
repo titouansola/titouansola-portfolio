6146200b59f31626830372f287d18d85146e4ce3
Titouan Sola <titouansola0@gmail.com>
2023-08-05 20:08:55 +0200

    V1 minimal animations

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 import Alpine from 'alpinejs';
+import recommendations from './data/recommendations.json';
 
 import './styles/index.css';
 
+
 // Alpine setup
 window.Alpine = Alpine;
+
+Alpine.data('recommendationState', () => ({
+    index: 0,
+    current: recommendations[0],
+    recommendationList: recommendations,
+
+    init() {
+        setInterval(() => {
+            this.index = (this.index + 1) % recommendations.length;
+            this.current = recommendations[this.index];
+        }, this.recommendationTimer);
+    },
+
+    get recommendationTimer() {
+        return parseInt(getComputedStyle(document.documentElement).getPropertyValue('--recommendation-timer').replace('ms', ''));
+    }
+}));
+
 Alpine.start();
